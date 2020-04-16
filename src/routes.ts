d1c9e2443727a86e20c321daf470bb0c8eb260b8
kielkow <matheuskiel@fiorifer.com.br>
2020-04-16 00:59:50 -0300

    Type objects and arrays

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,20 @@
 import { Request, Response } from 'express';
+import createUser from './services/CreateUser';
 
 export function helloWorld(request: Request, response: Response) {
+  const user = createUser(
+    { 
+      name: 'Matheus', 
+      email: 'matheus@fiorifer.com.br', 
+      password: '123456',
+      techs: [
+        'NodeJS', 
+        'ReactJS', 
+        'React Native',
+        { title: 'Javascript', experience: 100 },
+      ],
+    }
+  );
+
   return response.json({ message: 'Hello World' });
 }
